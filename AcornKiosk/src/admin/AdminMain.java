5b51b5d70b633cf3370832b400b0fb3d68f781b5
dhdhg <dhdhg@DAHEE>
2023-02-10 15:41:20 +0900

    ddd

--- AcornKiosk/src/admin/AdminMain.java
+++ AcornKiosk/src/admin/AdminMain.java
@@ -1,7 +1,32 @@
 package admin;
 
-public class AdminMain {
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AdminMain extends Application{
 public static void main(String[] args) {
+	launch(args);
+}
+
+@Override
+public void start(Stage primaryStage) throws Exception {
+	// TODO Auto-generated method stub
+	FXMLLoader loader = new FXMLLoader(
+			getClass().getResource("service/1_adminMenuUI.fxml"));
+	
+	Parent root = loader.load();
+	
+	Controller ctrl = loader.getController();
+	
+	ctrl.setRoot(root);
+	
+	primaryStage.setScene(new Scene(root));
+	primaryStage.setTitle("관리자모드");
+	primaryStage.show();
 	
 }
 }
--- AcornKiosk/src/admin/Controller.java
+++ AcornKiosk/src/admin/Controller.java
@@ -1,5 +1,10 @@
-package admin.service;
+package admin;
+
+import javafx.scene.Parent;
 
 public class Controller {
-
+	private Parent root;
+public void setRoot(Parent root) {
+	this.root = root;
+}
 }
