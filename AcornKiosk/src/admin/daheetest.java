420f56ed535b7d78dda126f4e3dc022b385ac929
dhdhg <dhdhg@DAHEE>
2023-02-10 10:39:50 +0900

    ddddd

--- AcornKiosk/src/admin/daheetest.java
+++ AcornKiosk/src/admin/daheetest.java
@@ -1,8 +1,18 @@
 package admin;
 
-public class daheetest {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class daheetest extends Application{
 public static void main(String[] args) {
 	System.out.println("daheetest");
+	launch(args);
+}
+
+@Override
+public void start(Stage primaryStage) throws Exception {
+	// TODO Auto-generated method stub
+	
 }
 
 }
