e823104bcb2a488aecbd595d8fc111f18ebe4d83
younssue <love_rvd7@naver.com>
2023-02-14 11:16:26 +0900

    장바구니 데이터테이블 구현

---
+++ AcornKiosk/src/order/db/orderMenu.java
@@ -0,0 +1,35 @@
+package order.db;
+
+public class orderMenu {
+	int menuid;
+	String menuname;
+	//주문가격 + 유료/무료옵션 가격도 추가
+	int menuprice;
+	//판매갯수
+	int menusell;
+	public int getMenuid() {
+		return menuid;
+	}
+	public void setMenuid(int menuid) {
+		this.menuid = menuid;
+	}
+	public String getMenuname() {
+		return menuname;
+	}
+	public void setMenuname(String menuname) {
+		this.menuname = menuname;
+	}
+	public int getMenuprice() {
+		return menuprice;
+	}
+	public void setMenuprice(int menuprice) {
+		this.menuprice = menuprice;
+	}
+	public int getMenusell() {
+		return menusell;
+	}
+	public void setMenusell(int menusell) {
+		this.menusell = menusell;
+	}
+
+}
---
+++ AcornKiosk/src/order/db/orderdata.java
@@ -0,0 +1,7 @@
+package order.db;
+
+import java.util.List;
+
+public interface orderdata {
+List<orderMenu> selectMenu();
+}
