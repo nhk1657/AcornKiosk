c8941f4e0b2f7b0a449473ef4ef09a78464b332f
현경 <nhk1657@kpu.ac.kr>
2023-02-14 18:55:14 +0900

    로그인 및 회원 가입 기능 추가

---
+++ AcornKiosk/src/member/service/LoginService.java
@@ -0,0 +1,8 @@
+package member.service;
+
+import javafx.scene.Parent;
+
+public interface LoginService {
+	public void loginProc(Parent loginRoot) throws Exception;
+	public void registerProc(Parent loginRoot);
+}
---
+++ AcornKiosk/src/member/service/MemberService.java
@@ -0,0 +1,7 @@
+package member.service;
+
+import javafx.scene.Parent;
+
+public interface MemberService {
+	public void joinProc(Parent registerRoot) throws Exception;
+}
