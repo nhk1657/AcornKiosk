280c637a94dd55ecb2e7f7498b84505a43299dba
dhdhg <dhdhg@DAHEE>
2023-02-14 11:07:54 +0900

    뒤로가기 버튼 구현

--- AcornKiosk/src/admin/service/AdminService.java
+++ AcornKiosk/src/admin/service/AdminService.java
@@ -3,15 +3,21 @@ package admin.service;
 
 
 
+import java.io.IOException;
+
 import javafx.scene.Parent;
 
 public interface AdminService {
 void menuManage(Parent root);
-void menuEdit(Parent root);
+//void menuEdit(Parent root);
 void sales(Parent root);
 void membermManage(Parent root);
-void memberEdit(Parent root);
+//void memberEdit(Parent root);
 void couponManage(Parent root);
 void endSystem(Parent root);
+void menuback(Parent root);
+void saleback(Parent root);
+void memberback(Parent member);
+void couponback(Parent coupon);
 
 }
